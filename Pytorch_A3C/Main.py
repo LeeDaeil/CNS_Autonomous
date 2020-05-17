612bf13cadf0a3c2148cff6695707b75aaa5812c
DaeilLee <dleodlf1004@naver.com>
2020-05-17 16:18:07 +0900

    Pytorch 뼈대 완성

--- Pytorch_A3C/Main.py
+++ Pytorch_A3C/Main.py
@@ -5,33 +5,80 @@ import torch.multiprocessing as mp
 import time
 import datetime
 import numpy as np
+from copy import copy
 from collections import deque
 
 
-class Work_info:  # 데이터 저장 및 초기 입력 변수 선정함.
+class Work_info:  # 데이터 저장 및 초기 입력 변수 선정
     def __init__(self):
-        self.Act_D = 3
-        self.State_D = len(self.state_para_list())
-        self.State_t = 12
-
         self.CURNET_COM_IP = '192.168.0.10'
         self.CNS_IP_LIST = ['192.168.0.9', '192.168.0.7', '192.168.0.4']
         self.CNS_PORT_LIST = [7100, 7200, 7300]
         self.CNS_NUMBERS = [2, 0, 0]
 
-    def state_para_list(self):  # 네트워크 입력 변수의 리스트를 반환
-        para_list = [
-            'KCNTOMS',   # 시간
-            'UAVLEGM',   # 평균온도
-            'UAVLEGS',   # Ref 온도
-            'ZINST63',   # VCT level
-            'ZVCT',      # VCT Level
-        ]
-        return para_list
+        self.DB_dict = {
+            'Buf': {
+                'S': [], 'R': [], 'A': []
+            },
+            'Act': 0,
+            'Net_S': {
+                'Time': {'key': 'KCNTOMS', 'v': 0},
+                'Temp_ref': {'key': 'UAVLEGM', 'v': 0},
+                'Temp_avg': {'key': 'UAVLEGS', 'v': 0},
+                'VCT_level': {'key': 'ZINST63', 'v': 0},
+                'VCT_pressure': {'key': 'ZVCT', 'v': 0},
+                'VCT_half': {'key': '-', 'v': 0},
+            },
+            'DB': {
+                'Act': [], 'Test_val': []
+            }
+        }
+        # copy
+        for _ in self.DB_dict['Net_S'].keys():
+            self.DB_dict['DB'][_] = []
+        self.init_DB_dict = copy(self.DB_dict)
+
+        self.Act_D = 3
+        self.State_D = len(self.DB_dict['Net_S'].keys())
+        self.State_t = 12
+        #
 
     def make_s(self, mem):      # 네트워크 입력 변수의 리스트에 상응하는 S 반환
-        para_list = self.state_para_list()
-        return [mem[para]['Val'] for para in para_list]
+        # 1) mem 에서 업데이트
+        for val in self.DB_dict['Net_S'].keys():
+            if val in mem.keys():
+                self.DB_dict['Net_S'][val]['v'] = mem[self.DB_dict['Net_S'][val]['key']]['Val']
+
+        # 2) Overwrite
+        self.DB_dict['Net_S']['VCT_half']['v'] = self.DB_dict['Net_S']['VCT_pressure']['v']/2
+
+        # 3) Save DB
+        self.DB_dict['DB']['Act'].append(self.DB_dict['DB']['Act'])
+
+        return [self.DB_dict['Net_S'][_]['v'] for _ in self.DB_dict['Net_S'].keys()]
+
+    # SAVE 모듈
+    def add_act_save_val(self, act):    # 제어 변수 저장
+        self.DB_dict['Act'] = act
+        return 0
+
+    def dump_save_val(self):            # 저장된 데이터 CSV 저장
+        pass
+
+    def init_save_val(self):            # 저장용 변수 초기화
+        self.DB_dict = self.init_DB_dict
+        return 0
+
+    # 버퍼
+    def init_buf(self):                 # 버퍼용 변수 초기화
+        self.DB_dict['Buf'] = {'S': [], 'R': [], 'A': []}
+        return 0
+
+    def append_buf(self, s, r, a): # 값 저장후 각 버퍼의 길이 반환
+        self.DB_dict['Buf']['S'].append(s)
+        self.DB_dict['Buf']['R'].append(r)
+        self.DB_dict['Buf']['A'].append(a)
+        return [len(self.DB_dict['Buf'][key]) for key in self.DB_dict['Buf'].keys()]
 
 
 class Worker(mp.Process):
@@ -48,7 +95,6 @@ class Worker(mp.Process):
     def run(self):
         while True:
             self.CNS.init_cns(initial_nub=17)
-            buffer_s, buffer_a, buffer_r = [], [], []
             ep_r = 0
             set_s = deque(maxlen=self.W_info.State_t)
 
@@ -60,10 +106,12 @@ class Worker(mp.Process):
 
             for i in range(1000):
                 # input-> action -> act_val : 입력 넣고 네트워크에서 나온 값 int로 치환
-                a, prob_a = self.L_net.choose_action_fin(torch.FloatTensor(set_s).view(1, self.W_info.State_t, self.W_info.State_D))
+                _input_state = torch.FloatTensor(set_s).view(1, self.W_info.State_t, self.W_info.State_D)
+                a, prob_a = self.L_net.choose_action_fin(_input_state)
 
-                # 액션을 보내기
+                # 액션을 보내기 및 저장
                 self.send_action(action=a)
+                self.W_info.add_act_save_val(act=a)
 
                 # 1회 Run
                 self.CNS.run_freeze_CNS()
@@ -73,10 +121,8 @@ class Worker(mp.Process):
                 # 평가
                 done, r = self.estimate_state()
 
-                # 값 저장
-                buffer_s.append(set_s)
-                buffer_a.append(prob_a)
-                buffer_r.append(r)
+                # 값 저장후 각 버퍼의 길이 리스트 반환
+                [Leg_s, Leg_r, Leg_a] = self.W_info.append_buf(s=set_s, r=r, a=prob_a)
 
                 if self.CNS.mem['KCNTOMS']['Val'] > 120:
                     print('DONE')
@@ -84,9 +130,9 @@ class Worker(mp.Process):
 
                 set_s.append(self.W_info.make_s(self.CNS.mem))  # S 만들고 저장
 
-                if np.shape(buffer_s)[0] >= 5:
+                if Leg_s >= 5:
                     print('Train!')
-                    self.push_and_pull(self.G_opt, self.L_net, self.G_net, done, set_s, buffer_s, buffer_a, buffer_r)
+                    self.push_and_pull(self.G_opt, self.L_net, self.G_net, done, set_s, self.W_info.DB_dict['Buf'])
                     buffer_s, buffer_a, buffer_r = [], [], []
 
                     if done:
@@ -120,7 +166,8 @@ class Worker(mp.Process):
         print(r)
         return done, r
 
-    def push_and_pull(self, opt, lnet, gnet, done, s_, bs, ba, br):
+    def push_and_pull(self, opt, lnet, gnet, done, s_, buf):
+        bs, ba, br = buf['S'], buf['A'], buf['R']
         gamma = 0.001
         if done:
             v_s_ = 0.0      # terminal
